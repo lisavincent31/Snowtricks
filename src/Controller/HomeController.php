a74a91c6f873f51a10800729d008a8daff9d30ea
lisavincent31 <lisa.vincent31150@gmail.com>
2024-01-26 06:16:56 +0100

    Initial commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $title = "Hello World";
         return $this->render('home/index.html.twig', [
-            'title' => $title,
+            'title' => "Accueil",
         ]);
     }
 }
