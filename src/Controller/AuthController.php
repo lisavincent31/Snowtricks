ff2eccc7244fd7f030c5126aa7b448ea629e56ef
lisavincent31 <lisa.vincent31150@gmail.com>
2024-02-01 16:56:04 +0100

    create register function with hashed password

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
-use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\UsersType;
+use App\Entity\Users;
 
 class AuthController extends AbstractController
 {
@@ -24,8 +25,55 @@ class AuthController extends AbstractController
 
     // Return the register view
     #[Route('/register', name: 'app_register')]
-    public function register(): Response
+    public function register(Request $request, 
+                            EntityManagerInterface $entityManager, 
+                            SluggerInterface $slugger,
+                            UserPasswordHasherInterface $passwordHasher
+                            ): Response
     {
-        return $this->render('auth/register.html.twig');
+        // create a user instance
+        $user = new Users();
+        // create the user form
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+        // validation after the form submit button
+        if($form->isSubmitted() && $form->isValid()) {
+            // register user avatar
+            $avatar = $form->get('avatar')->getData();
+            if($avatar) {
+                $originalFilename = pathinfo($avatar->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$avatar->guessExtension();
+
+                // move the file to the directory where avatars are stored
+                try{
+                    $avatar->move(
+                        $this->getParameter('medias.avatars_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+
+                }
+                $user->setAvatar($newFilename);
+
+                // hash password before save
+                $plainPassword = $form->get('password')->getData();
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+                // set is_verified to false
+                $user->setIsVerified(false);
+                // set the user role
+                $roles[] = 'ROLE_USER';
+                $user->setRoles($roles);
+                // save into the database
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+            return $this->redirectToRoute('app_home');
+        }
+        // return the view
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
