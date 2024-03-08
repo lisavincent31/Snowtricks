a038daf9052ddd112a1e591ffec83aef5f470583
lisavincent31 <lisa.vincent31150@gmail.com>
2024-03-08 08:20:36 +0100

    create comment and retrieve it into the trick detail page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TrickRepository;
+use App\Repository\MediaRepository;
 
 class HomeController extends AbstractController
 {
